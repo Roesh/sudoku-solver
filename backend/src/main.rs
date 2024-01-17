e21b7700adc03a5c5b624c4676e97134d010ea4b
Roshan <me@roshan.page>
2024-01-16 22:43:01 -0500

    feat: add tokio tungstenite

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,6 +2,7 @@ mod utils;
 mod sudoku_board;
 mod tests;
 
+//https://github.com/snapview/tokio-tungstenite/blob/master/examples/echo-server.rs
 fn main() {
 	utils::solve("test-boards/valid-final.txt");
 }
